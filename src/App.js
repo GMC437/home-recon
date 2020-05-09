6d8bc09e502b915a4a33fb01b1ffb18b716be466
Grant <grantmcnally.dev@gmail.com>
2020-05-09 18:53:35 +0100

    added deploy job to circle ci config

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
+          updated with circle ci!
         </a>
       </header>
     </div>
